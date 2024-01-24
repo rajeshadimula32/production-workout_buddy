e058eb5b23bafa95a4c985378a1d2d0d5c16c5b7
Adimula Rajesh <rajeshadimula@gmail.com>
2024-01-24 17:39:09 +0530

    removed some console log statements

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+      
     })
   })
   .catch((error) => {
-    console.log("error occured while connectiong mongodb")
+    // console.log("error occured while connectiong mongodb")
     console.log(error)
   })
